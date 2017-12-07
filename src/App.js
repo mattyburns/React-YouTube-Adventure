a1ca1424b1396cbbe2d764548a101766f8851bd2
Matty Burns <matty@Mattys-MacBook-Air.local>
2017-12-07 14:25:17 -0500

    YouTube Component with options

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import YouTube from 'react-youtube';
+
 
 class App extends Component {
+
   render() {
+    const opts = {
+     height: '390',
+     width: '640',
+     playerVars: {
+       autoplay: 1
+     }
+   };
     return (
       <div className="App">
         <header className="App-header">
@@ -13,6 +23,13 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <YouTube
+          videoId="2g811Eo7K8U"
+          opts={opts}
+        />
+
+        
       </div>
     );
   }
